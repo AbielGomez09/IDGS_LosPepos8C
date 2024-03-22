7ea197658a35b32a3a4b0adc4687f9940531d550
AbielGomez09 <156268416+AbielGomez09@users.noreply.github.com>
2024-03-21 19:44:44 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,27 +1,9 @@
 <?php
 
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+require_once "controllers/controller.php";
+require_once "models/enlaces.php";
+require_once "models/crud.php";
 
-function sayHello($name) {
-	echo "Hello $name!";
-}
-
+$mvc = new MvcController();
+$mvc -> pagina();
 ?>
-
-<html>
-	<head>
-		<title>Visual Studio Code Remote :: PHP</title>
-	</head>
-	<body>
-		<?php 
-		
-		sayHello('remote world');
-			
-		phpinfo(); 
-			
-		?>
-	</body>
-</html>
